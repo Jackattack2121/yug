24aad9d5a12b68144d81bc993fdd2abc6d5269ab
Jackattack2121 <186578331+Jackattack2121@users.noreply.github.com>
2025-11-24 01:19:57 +1030

    Remove hardcoded project details from HeaderWrapper component

--- components/layout/HeaderWrapper.tsx
+++ components/layout/HeaderWrapper.tsx
@@ -1,13 +1,6 @@
 import Header from './Header';
 
-const projects = [
-  { name: 'Mick Well REE Project', slug: 'mick-well' },
-  { name: 'Arthur River: LK1 REE Project', slug: 'arthur-river' },
-  { name: 'Chalby Chalby Lithium Project', slug: 'chalby-chalby' },
-  { name: 'NSW Projects', slug: 'nsw-projects' },
-];
-
 export default function HeaderWrapper() {
-  return <Header projects={projects} />;
+  return <Header />;
 }
 
